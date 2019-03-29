cd5456cef776e3acd1ab7e8bfd2196590674e745
zaid <monis.z.786@gmail.com>
2019-03-29 16:14:36 +0530

    Test Commit

--- app/src/main/java/textspeech/thezaxis/emandi/SignIn.java
+++ app/src/main/java/textspeech/thezaxis/emandi/SignIn.java
@@ -2,14 +2,12 @@ package textspeech.thezaxis.emandi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hello There!", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_sign_in);
     }
 }
