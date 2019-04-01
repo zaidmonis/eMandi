85aed03db70dea7f1023d4e288823ad5054e5355
pooja <poojaarora0901@gmail.com>
2019-04-01 23:17:15 +0530

    Commit2 By Pooja

--- app/src/main/java/textspeech/thezaxis/emandi/HomeActivity.java
+++ app/src/main/java/textspeech/thezaxis/emandi/HomeActivity.java
@@ -71,7 +71,7 @@ public class HomeActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.open, R.string.close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
