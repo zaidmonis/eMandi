e4f14f32a446184afcb96ad340e10c08b2791ed3
pooja <poojaarora0901@gmail.com>
2019-04-01 22:21:13 +0530

    Commit By Pooja

---
+++ app/src/main/java/textspeech/thezaxis/emandi/YouTubeVideos.java
@@ -0,0 +1,17 @@
+package textspeech.thezaxis.emandi;
+
+public class YouTubeVideos {
+
+    String videoUrl;
+    public YouTubeVideos(){}
+
+    public YouTubeVideos(String videoUrl){this.videoUrl=videoUrl;}
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
